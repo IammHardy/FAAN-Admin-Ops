250388276d0f5e3d87cb407e2b53c5bffe89376c
iammhardy <iamhardyusuf@gmail.com>
2026-05-17 10:51:01 +0100

    Test Sentry from dashboard

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,12 +7,7 @@ Sentry.init do |config|
   ]
 
   config.send_default_pii = true
-
   config.environment = Rails.env
-
-  # Only run in production
   config.enabled_environments = %w[production]
-
-  # Capture only some traces (better for production)
   config.traces_sample_rate = 0.2
 end
